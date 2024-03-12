6d6d93fb4d2758999817e425527b0a5cc5753d94
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2024-03-13 05:24:21 +0530

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,18 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[text,setText]= useState("");
+
+  function whatText(e){
+    setText(Hello ${e.target.value}!);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Enter your name:</h1>
+        <input type="text" onChange={whatText}></input>
+        <p>{text}</p>
     </div>
   )
 }
